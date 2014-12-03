e7d1ea653102fa8726ffbfc8b347c341e27cbe46
Souheil CHELFOUH <trollfot@gmail.com>
2014-12-03 23:05:55 +0100

    Added interfaces

---
+++ src/uvc/entities/browser/managers.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from cromlech.browser import IViewSlot
+
+
+class IPageTop(IViewSlot):
+    """Marker For the area that sits at the top of the page.
+    """
+
+
+class ITabs(IViewSlot):
+    """Marker for the action tabs.
+    """
+
+
+class IAboveContent(IViewSlot):
+    """Marker For the area that sits above the page body.
+    """
+
+
+class IBelowContent(IViewSlot):
+    """Marker For the area that sits under the page body.
+    """
+
+
+class IHeaders(IViewSlot):
+    """Marker For Headers
+    """
+
+
+class IToolbar(IViewSlot):
+    """Marker for Toolbar
+    """
+
+
+class IFooter(IViewSlot):
+    """
+    """
+
+
+class IExtraInfo(IViewSlot):
+    """
+    """
---
+++ src/uvc/entities/browser/menus.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from dolmen.menu.interfaces import IMenu
+
+
+class IGlobalMenu(IMenu):
+    """Marker for GlobalMenu
+    """
+
+
+class INavigationMenu(IMenu):
+    """Marker for NavigationMenu
+    """
+    
+
+class IPersonalPreferences(IMenu):
+    """Marker for PersonalPreferences
+    """
+
+
+class IFooterMenu(IMenu):
+    """Marker for Footer
+    """
+
+
+class IDocumentActions(IMenu):
+    """Marker for DocumentActions
+    """
+
+    
+class IAddMenu(IMenu):
+    """Marker for the Add Menu
+    """
+    
+
+class IExtraViews(IMenu):
+    """Marker for additional Views for Folders
+       Objects etc...
+    """
+
+
+class IPersonalMenu(IMenu):
+    """Marker for PersonalMenu
+    """
+
+
+class IContextualActionsMenu(IMenu):
+    """Marker for PersonalMenu
+    """
+
+
+class IUserMenu(IMenu):
+    """Marker for the UserMenu
+    """
+
+
+class ISubMenu(IMenu):
+    pass
